3c29313eeb52f79575c7f101a4f71949bca46440
ViscondeDev <willianoacpf@gmail.com>
2026-07-26 11:32:46 -0300

    particles

--- scenes/piece/piece.gd
+++ scenes/piece/piece.gd
@@ -71,4 +71,5 @@ func get_taken() -> void:
 	elif Board.current_board.pieces.size() == 2:
 		Level.current.end_game(Level.State.WON)
 	Board.current_board.pieces.erase(current_board_position)
-	visible = false
+	sprite.visible = false
+	$DeathParticles.emitting = true
