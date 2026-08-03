2911372d5386abb7ce66e5e05f7e554ce6de5912
ViscondeDev <willianoacpf@gmail.com>
2026-08-03 17:12:54 -0300

    final version

--- core/looping_sections.gd
+++ core/looping_sections.gd
@@ -11,24 +11,56 @@ extends Node
 var playing: bool
 
 
+func _ready() -> void:
+	var volume = loop.volume_db
+	loop.volume_db = -80
+	loop.play()
+	await get_tree().create_timer(0.1).timeout
+	loop.stop()
+	loop.volume_db = volume
+
+	volume = loop_tail.volume_db
+	loop_tail.volume_db = -80
+	loop_tail.play()
+	await get_tree().create_timer(0.1).timeout
+	loop_tail.stop()
+	loop_tail.volume_db = volume
+
+	volume = intro_tail.volume_db
+	intro_tail.volume_db = -80
+	intro_tail.play()
+	await get_tree().create_timer(0.1).timeout
+	intro_tail.stop()
+	intro_tail.volume_db = volume
+
 func trigger() -> void:
 	if play_transition:
 		AudioManager.current.transition.play()
 		await get_tree().create_timer(1.8).timeout
 	intro.play()
-	await get_tree.create_timer(intro.stream.get_lenght())
+	playing = true
+	await get_tree().create_timer(intro.stream.get_length()).timeout
+	if playing == true:
+		_start_loop()
 
 
 func stop() -> void:
 	intro.stop()
 	loop.stop()
+	playing = false
 
 
 func _start_loop():
 	intro_tail.play()
 	loop.play()
+	await get_tree().create_timer(loop.stream.get_length()).timeout
+	if playing == true:
+		_loop_again()
 
 
 func _loop_again():
 	loop_tail.play()
 	loop.play()
+	await get_tree().create_timer(loop.stream.get_length()).timeout
+	if playing == true:
+		_loop_again()
